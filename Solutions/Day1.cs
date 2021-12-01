d4ec28b2dd290daadd897cdbfb8b607fb05ff8ee
HarryAmmon <harry_a99@hotmail.co.uk>
2021-12-01 13:28:21 +0000

    Add Day1 Part 1 solution

--- Solutions/Day1.cs
+++ Solutions/Day1.cs
@@ -6,7 +6,15 @@ namespace Solutions
     {
         public int Part1(string[] fileContent)
         {
-            throw new System.NotImplementedException();
+            int count = 0;
+            for (int i = 1; i < fileContent.Length; i++)
+            {
+                if (int.Parse(fileContent[i]) > int.Parse(fileContent[i - 1]))
+                {
+                    count++;
+                }
+            }
+            return count;
         }
 
         public int Part2(string[] fileContent)
