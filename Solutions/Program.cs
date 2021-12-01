70f06547cf9246bdb9ca52697222f9b7b33610c9
HarryAmmon <harry_a99@hotmail.co.uk>
2021-12-01 13:12:38 +0000

    Set up

---
+++ Solutions/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Solutions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
