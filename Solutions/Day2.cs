e15bdaebe45412a2b52ea4dec4cbaaa2d8cb8d0a
HarryAmmon <harry_a99@hotmail.co.uk>
2021-12-02 09:10:01 +0000

    Complete day2

--- Solutions/Day2.cs
+++ Solutions/Day2.cs
@@ -1,4 +1,6 @@
 using AdventOfCodeCore;
+using Solutions.Models;
+using Solutions.Services;
 
 namespace Solutions
 {
@@ -6,12 +8,24 @@ namespace Solutions
     {
         public int Part1(string[] fileContent)
         {
-            throw new System.NotImplementedException();
+            Position position = new Position();
+            INavigator nav = new BasicNavigator();
+            foreach (string command in fileContent)
+            {
+                position = nav.ExecuteCommand(command, position);
+            }
+            return position.Depth * position.Horizontal;
         }
 
         public int Part2(string[] fileContent)
         {
-            throw new System.NotImplementedException();
+            Position position = new Position();
+            INavigator nav = new AdvancedNavigator();
+            foreach (string command in fileContent)
+            {
+                position = nav.ExecuteCommand(command, position);
+            }
+            return position.Depth * position.Horizontal;
         }
     }
 }
---
+++ Solutions/Models/Position.cs
@@ -0,0 +1,9 @@
+namespace Solutions.Models
+{
+    public class Position
+    {
+        public int Horizontal { get; set; }
+        public int Depth { get; set; }
+        public int Aim { get; set; }
+    }
+}
