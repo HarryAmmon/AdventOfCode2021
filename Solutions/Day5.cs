0c25016b08181abf310a95c5bb134426e5016381
HarryAmmon <harry_a99@hotmail.co.uk>
2022-01-20 17:44:44 +0000

    Initial setup

---
+++ Solutions/Day5.cs
@@ -0,0 +1,18 @@
+using System;
+using AdventOfCodeCore;
+
+namespace Solutions
+{
+    public class Day5 : ISolution<int>
+    {
+        public int Part1(string[] fileContent)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int Part2(string[] fileContent)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- Solutions/Program.cs
+++ Solutions/Program.cs
@@ -6,11 +6,11 @@ namespace Solutions
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            string[] fileContent = File.ReadAllLines(@"Inputs/Day4.txt");
+            string[] fileContent = File.ReadAllLines(@"Inputs/Day5.txt");
 
-            ISolution<int> solutions = new Day4();
+            ISolution<int> solutions = new Day5();
 
             Console.WriteLine(solutions.Part1(fileContent));
             Console.WriteLine(solutions.Part2(fileContent));
